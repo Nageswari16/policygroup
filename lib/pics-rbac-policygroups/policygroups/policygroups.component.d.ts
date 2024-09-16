5e5242336a7d07388720a1843e81126432e471e0
Nageswari16 <153205021+Nageswari16@users.noreply.github.com>
2024-09-16 17:48:03 +0530

    se

--- lib/pics-rbac-policygroups/policygroups/policygroups.component.d.ts
+++ lib/pics-rbac-policygroups/policygroups/policygroups.component.d.ts
@@ -42,6 +42,7 @@ export declare class PolicygroupsComponent implements OnInit {
         [key: string]: import("@angular/forms").AbstractControl;
     };
     ngOnDestroy(): void;
+    errorMessage(formcontrolname: any, error: any): void;
     onInput(event: Event, formcontrolname: any, fieldtype: any, label: any, required: boolean): void;
     getPolicyGroupList(_managementGroupId?: number): void;
     getManagementGroupTree(organizationid: any): void;
